1522d9bdde193f4cdf690e77de5b3bbf0a8f1016
vvazquez <vdaniel.vazq@gmail.com>
2022-07-30 00:52:39 -0500

    Insertando botón contador

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import Counter from './components/Counter/Counter';
 
 function App() {
+  const handleOnAdd = (quantity) => {
+    console.log(`la cantidad agregada es: ${quantity}`)
+  }
   return (
     <div className="App">
       <header className="App-header">
         <Navbar />
       </header>
       <ItemListContainer greeting= 'Bienvenido a la tienda de Funkos' />
+      <Counter stock={10} onAdd={handleOnAdd}/>
     </div>
   );
 }
